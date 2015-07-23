49dae62479e83a2d7d11c3492a7c7b9299474164
jayakumar.srinivasan <jayakumar.srinivasan@edorasware.com>
2015-07-23 21:35:48 +0530

    23July2015

--- Sample2/app/app.js
+++ Sample2/app/app.js
@@ -1,8 +1,40 @@
 var app = angular.module('myApp', []);
-app.directive('helloWorld',function(){
+app.directive('mainMenu',function(){
 	return {
-		restrict: 'AE',
+		restrict: 'ACE',
 		replace: true,
-		template: '<h3>Hello world</h3>'
+		transclude: 'element',
+		scope: true,
+		template: [
+			'<ul>',	
+				'<li rel="{{menuItems.indexOf(item)}}">{{item.name}}</li>',
+			'</ul>'
+		].join(''),
+		compile: function(elem, attr){
+			var rpt = document.createAttribute('ng-repeat');
+			rpt.value = attr.element;
+			console.log(attr.element, elem);
+			elem[0].children[0].attributes.setNamedItem(rpt);
+			
+			var link = function(scope, elem, attr){			
+				console.log(elem.children('li'));
+			}
+			return link;
+		}
 	};
+});
+
+app.directive('mainMenu',function(){
+	return {
+		restrict: 'ACE',		
+		scope: true,
+		link: function(){console.log("link",arguments);},
+		controller: function(){console.log("controller",arguments);}
+	};
+});
+
+app.controller('mainMenuCtrl',function($scope){
+	$scope.menuItems = [
+		{"name": "aa"}, {"name":"bb"}, {"name":"cc"}
+	];
 });
